ddc887710a60dbafb5140e67b8d041b80092c462
Saturnin Dao <saturnindao@gmail.com>
2020-09-15 16:59:37 +0000

    firebase function for mail en cours

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,9 @@
 const functions = require('firebase-functions');
-const admin=require('firebase-admin');
-const nodemailer =required('nodemailer');
 
-admin.initializeApp()
-require('dotenv').config()
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 
-
-const {SENDER_EMAIL,SENDER_PASSWORD}= process.env;
-
-exports.sendEmailNotification=functions.firestore.document('submissions/{docId}')
-.onCreate((snap,ctx)=>{
-    const data=snap.data();
-
-    let authData=nodemailer.createTransport({
-        host:'smtp.gmail.com',
-        port:465,
-        secure:true,
-        auth:{
-            user:SENDER_EMAIL,
-            pass:SENDER_PASSWORD
-        }
-    });
-
-    authData.sendMail({
-        from :'info.truly@makethatap.com',
-        to:`${data.email}`,
-        subject:'Your are submission info to datepe',
-        text:`${data.email}`,
-        html:`${data.email}`,
-    }).then(res=>console.log('successfully that mail')).catch(err=>console.log(err));
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
 });
